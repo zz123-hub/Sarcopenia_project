5f59cf4df5caa3c0ebda9798a3b9c5bf6a38e7ca
ZZ <57436632+zz123-hub@users.noreply.github.com>
2024-08-01 15:21:28 +0800

    Update PCA.R

--- Script and code/PCA.R
+++ Script and code/PCA.R
@@ -1,5 +1,26 @@
-aaa <- read.table("222.txt",header = T,row.names = 1,sep="\t")
+library(ggord)
 library(ggplot2)
+pca_data=read.table("abundance.txt",header=T,sep="\t",row=1)
+pca1=prcomp(pca_data,center=TRUE,retx=T,scale=T)
+summary(pca1)
+plot(pca1$x, main="after PCA")
+head(pca1$x)
+write.csv(pca1$x,'pc.csv', quote = FALSE) 
+rm1<-cor(pca_data)
+rs1<-eigen(rm1)
+val <- rs1$values
+(Standard_deviation <- sqrt(val))
+(Proportion_of_Variance <- val/sum(val))
+
+#####################
+library(vegan)
+group_info <- read.table("group.txt", header = TRUE, sep = "\t")
+distance_matrix <- dist(pca1$x)
+anosim_result <- anosim(distance_matrix, group_info$Group)
+print(anosim_result)
+
+#####################
+aaa <- read.table("222.txt",header = T,row.names = 1,sep="\t")
 ggplot(aaa,aes(x=PC1,y=PC2,colour=group))+
   geom_point(alpha =.7, size=4)+scale_size_area()+stat_ellipse(level=0.5,mapping= aes(colour=group))+
   scale_colour_brewer(type = "div", palette = "Dark2")+theme_classic()+
